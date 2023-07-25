b78221d83132c56aec773e245a1d33f4f0fa218e
liussi <125599487+liussi@users.noreply.github.com>
2023-07-25 15:51:38 +0000

    Deploying to gh-pages from @ liussi/goit-js-hw-10@77b9f6e0b1afc29dd28770fdaecc3bd7e582fb4d 🚀

--- index.3ceed831.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".cat-info"),document.querySelector(".breed-select"),document.querySelector(".loader"),document.querySelector(".error");!function(){const e=fetch("https://api.thecatapi.com/v1/breeds").then((e=>e.json())).then();console.log(e)}();
-//# sourceMappingURL=index.3ceed831.js.map
